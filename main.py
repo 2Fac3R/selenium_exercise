53d1b60d63fdf83ed0800cd803fe92a6c81d90b3
2Fac3R <genr1818@gmail.com>
2021-02-22 14:47:24 -0600

    Updated

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     tasks = TaskHandler('https://randomtodolistgenerator.herokuapp.com/library')
     task_list = tasks.get_tasks()
 
-    tasks.login('https://todoist.com/users/showlogin','genr1818@gmail.com', 'pythonselenium')
+    tasks.login('https://todoist.com/users/showlogin','email@gmail.com', 'password')
     tasks.add_tasks(task_list)
 
   finally:
